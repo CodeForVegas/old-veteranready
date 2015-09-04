c4ee52cbf938bfb0bcd23d53e3c52c76410473a8
Shawn Looker <shawn.looker@gmail.com>
2015-09-03 23:18:09 -0700

    Initial commit.  Using handlebars, bootstrap and jquery.

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var routes = require('./routes/index');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(express.static('public'));
+app.use('/css',express.static(__dirname+'/public/css'));
+app.use('/js',express.static(__dirname+'/public/js'));
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', routes);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
