7c336aa6f677b1b695fb6a417754547bb0e1e906
Shawn Looker <shawn.looker@gmail.com>
2015-09-05 00:30:12 -0700

    Header almost done.  Buttons need work and I need to round the corners of the white part.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app = express();
 app.use(express.static('public'));
 app.use('/css',express.static(__dirname+'/public/css'));
 app.use('/js',express.static(__dirname+'/public/js'));
+app.use('/images',express.static(__dirname+'/public/images'));
 app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
